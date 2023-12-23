e8d176bfc2d620d6ad272e26e232172de1faac45
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-12-23 10:09:16 +0530

    set milvus storage trait and set creation and insert of collections

--- quester/storage/src/storage.rs
+++ quester/storage/src/storage.rs
@@ -1,5 +1,64 @@
+use std::{fmt, sync::Arc};
+
 use async_trait::async_trait;
 use common::{SemanticKnowledgePayload, VectorPayload};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+/// Storage error kind.
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum StorageErrorKind {
+	/// Error in collection creation.
+	CollectionCreation,
+	/// Error in collection building.
+	CollectionBuilding,
+	/// Insertion error.
+	Insertion,
+	/// The target index does not exist.
+	NotFound,
+	/// The request credentials do not allow for this operation.
+	Unauthorized,
+	/// A third-party service forbids this operation, or is misconfigured.
+	Service,
+	/// Any generic internal error.
+	Internal,
+	/// A timeout occurred during the operation.
+	Timeout,
+	/// Io error.
+	Io,
+}
+
+/// Generic StorageError.
+#[derive(Debug, Clone, Error)]
+#[error("storage error(kind={kind:?}, source={source})")]
+#[allow(missing_docs)]
+pub struct StorageError {
+	pub kind: StorageErrorKind,
+	#[source]
+	pub source: Arc<anyhow::Error>,
+}
+
+/// Generic Result type for storage operations.
+pub type StorageResult<T> = Result<T, StorageError>;
+
+impl StorageError {
+	/// Add some context to the wrapper error.
+	pub fn add_context<C>(self, ctx: C) -> Self
+	where
+		C: fmt::Display + Send + Sync + 'static,
+	{
+		StorageError {
+			kind: self.kind,
+			source: Arc::new(anyhow::anyhow!("{ctx}").context(self.source)),
+		}
+	}
+
+	/// Returns the corresponding `StorageErrorKind` for this error.
+	pub fn kind(&self) -> StorageErrorKind {
+		self.kind
+	}
+}
+
 /// Storage is a trait for all storage types.
 /// Currently we support Graph, Vector and Index storages.
 #[async_trait]
@@ -8,11 +67,11 @@ pub trait Storage: Send + Sync + 'static {
 	async fn check_connectivity(&self) -> anyhow::Result<()>;
 
 	/// Insert VectorPayload into storage
-	async fn insert_vector(&self, payload: Vec<(String, VectorPayload)>) -> anyhow::Result<()>;
+	async fn insert_vector(&self, payload: Vec<(String, VectorPayload)>) -> StorageResult<()>;
 
 	/// Insert SemanticKnowledgePayload into storage
 	async fn insert_graph(
 		&self,
 		payload: Vec<(String, SemanticKnowledgePayload)>,
-	) -> anyhow::Result<()>;
+	) -> StorageResult<()>;
 }
--- quester/storage/src/vector/milvus/milvus_storage.rs
+++ quester/storage/src/vector/milvus/milvus_storage.rs
@@ -1,9 +1,14 @@
 use std::sync::Arc;
 
 use common::{SemanticKnowledgePayload, VectorPayload};
-use milvus::client::Client as MilvusClient;
+use milvus::{
+	client::Client as MilvusClient,
+	data::FieldColumn,
+	schema::{CollectionSchemaBuilder, FieldSchema},
+	value::ValueVec,
+};
 
-use crate::storage::Storage;
+use crate::{storage::Storage, StorageError, StorageErrorKind, StorageResult};
 use async_trait::async_trait;
 
 pub struct MilvusStorage {
@@ -15,10 +20,7 @@ impl MilvusStorage {
 	pub async fn new(url: String) -> Self {
 		let client_res = MilvusClient::new(url.clone()).await;
 		match client_res {
-			Ok(client) => {
-				log::info!("Milvus client created");
-				MilvusStorage { client: Arc::new(client), url }
-			},
+			Ok(client) => MilvusStorage { client: Arc::new(client), url },
 			Err(err) => {
 				log::error!("Milvus client creation failed: {:?}", err);
 				panic!("Milvus client creation failed: {:?}", err);
@@ -30,18 +32,136 @@ impl MilvusStorage {
 #[async_trait]
 impl Storage for MilvusStorage {
 	async fn check_connectivity(&self) -> anyhow::Result<()> {
-		let _ = self.client.clone().list_collections().await?;
+		let _ = self.client.list_collections().await?;
 		Ok(())
 	}
 
-	async fn insert_vector(&self, _payload: Vec<(String, VectorPayload)>) -> anyhow::Result<()> {
+	async fn insert_vector(&self, _payload: Vec<(String, VectorPayload)>) -> StorageResult<()> {
+		for (id, payload) in _payload {
+			let result = self.insert_or_create_collection(&payload.namespace, &id, &payload).await;
+			if let Err(err) = result {
+				log::error!("Vector insertion failed: {:?}", err);
+				return Err(err);
+			}
+		}
 		Ok(())
 	}
 
 	async fn insert_graph(
 		&self,
 		_payload: Vec<(String, SemanticKnowledgePayload)>,
-	) -> anyhow::Result<()> {
-		Err(anyhow::anyhow!("Not implemented"))
+	) -> StorageResult<()> {
+		// Your insert_graph implementation here
+		Ok(())
+	}
+}
+
+impl MilvusStorage {
+	async fn insert_or_create_collection(
+		&self,
+		collection_name: &str,
+		id: &str,
+		payload: &VectorPayload,
+	) -> StorageResult<()> {
+		let collection = self.client.get_collection(collection_name).await;
+		match collection {
+			Ok(collection) => {
+				log::debug!("Collection found: {:?}", collection);
+				self.insert_into_collection(&collection, id, payload).await
+			},
+			Err(_) => {
+				log::debug!("Collection not found, creating: {:?}", collection_name);
+				self.create_and_insert_collection(collection_name, id, payload).await
+			},
+		}
+	}
+
+	async fn insert_into_collection(
+		&self,
+		collection: &milvus::collection::Collection,
+		id: &str,
+		payload: &VectorPayload,
+	) -> StorageResult<()> {
+		let knowledge_field = FieldColumn::new(
+			collection.schema().get_field("knowledge").unwrap(),
+			ValueVec::String(vec![payload.id.clone()]),
+		);
+		let relationship_field = FieldColumn::new(
+			collection.schema().get_field("relationship").unwrap(),
+			ValueVec::String(vec![payload.namespace.clone()]),
+		);
+		let document_field = FieldColumn::new(
+			collection.schema().get_field("document").unwrap(),
+			ValueVec::String(vec![id.to_string()]),
+		);
+		let embeddings_field = FieldColumn::new(
+			collection.schema().get_field("embeddings").unwrap(),
+			payload.embeddings.clone(),
+		);
+
+		let records = vec![knowledge_field, relationship_field, document_field, embeddings_field];
+		let insert_result = collection.insert(records, None).await;
+		match insert_result {
+			Ok(insert_result) => {
+				log::debug!("Insert result: {:?}", insert_result);
+				Ok(())
+			},
+			Err(err) => {
+				log::error!("Insert failed: {:?}", err);
+				Err(StorageError {
+					kind: StorageErrorKind::Insertion,
+					source: Arc::new(anyhow::Error::from(err)),
+				})
+			},
+		}
+	}
+
+	async fn create_and_insert_collection(
+		&self,
+		collection_name: &str,
+		id: &str,
+		payload: &VectorPayload,
+	) -> StorageResult<()> {
+		let description = format!("Semantic collection adhering to s->p->o ={:?}", payload.id);
+		let new_coll = CollectionSchemaBuilder::new(collection_name, description.as_str())
+			.add_field(FieldSchema::new_primary_int64("id", "auto id for each vector", true))
+			.add_field(FieldSchema::new_string("knowledge", "subject, predicate, object"))
+			.add_field(FieldSchema::new_string(
+				"relationship",
+				"predicate associated with embedding",
+			))
+			.add_field(FieldSchema::new_string("document", "document associated with embedding"))
+			.add_field(FieldSchema::new_float_vector(
+				"embeddings",
+				"semantic vector embeddings",
+				payload.size as i64,
+			))
+			.build();
+
+		match new_coll {
+			Ok(new_coll) => {
+				let collection = self.client.create_collection(new_coll, None).await;
+				match collection {
+					Ok(collection) => {
+						log::debug!("Collection created: {:?}", collection);
+						self.insert_into_collection(&collection, id, payload).await
+					},
+					Err(err) => {
+						log::error!("Collection creation failed: {:?}", err);
+						Err(StorageError {
+							kind: StorageErrorKind::CollectionCreation,
+							source: Arc::new(anyhow::Error::from(err)),
+						})
+					},
+				}
+			},
+			Err(err) => {
+				log::error!("Collection builder failed: {:?}", err);
+				Err(StorageError {
+					kind: StorageErrorKind::CollectionBuilding,
+					source: Arc::new(anyhow::Error::from(err)),
+				})
+			},
+		}
 	}
 }
