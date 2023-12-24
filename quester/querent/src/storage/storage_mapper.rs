948d0e5ffbda299b5bf56a7975d906f4b9a01a30
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-12-24 11:26:59 +0530

    hookup graph database

--- quester/querent/src/storage/storage_mapper.rs
+++ quester/querent/src/storage/storage_mapper.rs
@@ -144,6 +144,23 @@ impl Handler<ContextualTriples> for StorageMapper {
 	) -> Result<(), ActorExitStatus> {
 		self.counters.increment_total(message.len() as u64);
 		self.counters.increment_event_count(message.event_type(), message.len() as u64);
+		let event_type = message.event_type();
+		let storage = self._event_storages.get(&event_type);
+		let storage_items = message.event_payload();
+		if let Some(storage) = storage {
+			let upsert_result = storage.insert_graph(storage_items).await;
+			match upsert_result {
+				Ok(()) => {
+					self.counters
+						.increment_event_to_storage(message.event_type(), message.len() as u64);
+				},
+				Err(e) => {
+					log::error!("Error while inserting graphs: {:?}", e);
+					return Err(ActorExitStatus::Failure(e.source));
+				},
+			}
+		}
+
 		Err(ActorExitStatus::Success)
 	}
 }
