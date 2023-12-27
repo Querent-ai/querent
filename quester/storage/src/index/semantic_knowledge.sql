f88bbe7f9656d89d68172d4c093b1eddb4acae4f
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-12-27 12:05:48 +0530

    add a table to store semantic knowledge

---
+++ quester/storage/src/index/semantic_knowledge.sql
@@ -0,0 +1,12 @@
+CREATE TABLE semantic_knowledge (
+    id SERIAL PRIMARY KEY,
+    subject VARCHAR,
+    subject_type VARCHAR,
+    object VARCHAR,
+    object_type VARCHAR,
+    predicate VARCHAR,
+    predicate_type VARCHAR,
+    sentence TEXT,
+    document_id VARCHAR,
+    timestamp TIMESTAMPTZ
+);
