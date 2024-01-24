dd8fad8d56ce7b5df7189f6ed2c829489d0d99fd
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-01-24 08:09:30 -0600

    remove all ui stuff

--- ui/src/routes/sidebarStore.js
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store";
-
-export const showCollectorComponent = writable(false);
-
-showCollectorComponent.subscribe((value) => {
-  console.log("showCollectorComponent:", value);
-});
--- ui/src/types/connectionDetailTypes.ts
+++
@@ -1,8 +0,0 @@
-// types.ts
-export type ConnectionDetailTypes = {
-  name: string;
-  id: string;
-  progress: number;
-};
-
-export type TableData = ConnectionDetailTypes[];
