ce78f5908ec304a90e5a1d5ddd359e4178003aa5
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-02-18 19:05:04 -0600

    workflow engine

--- quester/querent/src/lib.rs
+++ quester/querent/src/lib.rs
@@ -56,7 +56,7 @@ async def print_querent(config, text: str):
         import querent
         print("✨ Querent imported successfully ✨")
         querent_started = True
-        await querent.workflow.start_workflow(config)
+        await querent.workflow.start_workflow_engine(config)
         return
     except Exception as e:
         querent_started = False
