1a3ed9c762e046681d473a9696234eaf95b125b2
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-05-06 21:42:17 -0500

    add opendal gcs

--- quester/sources/src/source.rs
+++ quester/sources/src/source.rs
@@ -1,13 +1,19 @@
 use async_trait::async_trait;
-use futures_util::Stream;
 use serde::{Deserialize, Serialize};
 use std::{
 	fmt::{self, Debug},
+	io,
+	ops::Range,
+	path::Path,
 	sync::Arc,
 };
 use thiserror::Error;
+use tokio::io::{AsyncRead, AsyncWrite};
 
-use crate::CollectedBytes;
+pub trait SendableAsync: AsyncWrite + Send + Unpin {}
+impl<W: AsyncWrite + Send + Unpin> SendableAsync for W {}
+
+use crate::{default_copy_to_file, BlobPayload};
 
 /// Storage error kind.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
@@ -20,6 +26,10 @@ pub enum SourceErrorKind {
 	NotSupported,
 	/// Io error.
 	Io,
+	/// Not found error.
+	NotFound,
+	/// Unauthorized error.
+	Unauthorized,
 }
 
 /// Generic SourceError.
@@ -57,15 +67,69 @@ impl SourceError {
 	}
 }
 
+impl From<io::Error> for SourceError {
+	fn from(err: io::Error) -> SourceError {
+		match err.kind() {
+			io::ErrorKind::NotFound =>
+				SourceError::new(SourceErrorKind::NotFound, Arc::new(err.into())),
+			_ => SourceError::new(SourceErrorKind::Io, Arc::new(err.into())),
+		}
+	}
+}
 /// Sources is all possible data sources that can be used to create a `CollectedBytes`.
 #[async_trait]
-pub trait Source: Send + Sync + 'static {
+pub trait Source: fmt::Debug + Send + Sync + 'static {
 	/// Establishes a connection to the source.
-	async fn connect(&self) -> SourceResult<()>;
+	async fn check_connectivity(&self) -> anyhow::Result<()>;
 
-	/// Polls the source to collect data. Returns an iterator over collected bytes.
-	async fn poll(&self) -> SourceResult<Box<dyn Stream<Item = CollectedBytes> + Unpin + Send>>;
+	/// Pulls data from the source.
+	/// Path is of format `bucket_name/path/to/file`.
+	async fn put(&self, path: &Path, payload: Box<dyn BlobPayload>) -> SourceResult<()>;
 
-	/// Disconnects from the source.
-	async fn disconnect(&self) -> SourceResult<()>;
+	/// Pulls data from the source and copies it to a file.
+	async fn copy_to_file(&self, path: &Path, output_path: &Path) -> SourceResult<u64> {
+		default_copy_to_file(self, path, output_path).await
+	}
+
+	/// Downloads a slice of data from the source.
+	async fn get_slice(&self, path: &Path, range: Range<usize>) -> SourceResult<Vec<u8>>;
+
+	/// Downloads a slice of data from the source as a stream.
+	async fn get_slice_stream(
+		&self,
+		path: &Path,
+		range: Range<usize>,
+	) -> SourceResult<Box<dyn AsyncRead + Send + Unpin>>;
+
+	/// Downloads the entire content of a "small" file, returns an in memory buffer.
+	/// For large files prefer `copy_to_file`.
+	async fn get_all(&self, path: &Path) -> SourceResult<Vec<u8>>;
+
+	async fn exists(&self, path: &Path) -> SourceResult<bool> {
+		match self.file_num_bytes(path).await {
+			Ok(_) => Ok(true),
+			Err(storage_err) if storage_err.kind() == SourceErrorKind::NotFound => Ok(false),
+			Err(other_storage_err) => Err(other_storage_err),
+		}
+	}
+
+	/// Returns a file size.
+	async fn file_num_bytes(&self, path: &Path) -> SourceResult<u64>;
+
+	fn copy_to<'life0, 'life1, 'life2, 'async_trait>(
+		&'life0 self,
+		path: &'life1 Path,
+		output: &'life2 mut dyn SendableAsync,
+	) -> ::core::pin::Pin<
+		Box<
+			dyn ::core::future::Future<Output = SourceResult<()>>
+				+ ::core::marker::Send
+				+ 'async_trait,
+		>,
+	>
+	where
+		'life0: 'async_trait,
+		'life1: 'async_trait,
+		'life2: 'async_trait,
+		Self: 'async_trait;
 }
