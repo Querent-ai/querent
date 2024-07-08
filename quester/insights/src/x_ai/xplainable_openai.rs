c7660f60f21c201ca50ea372a57eada962973481
ngupta10 <ngupta10.slb@gmail.com>
2024-07-08 11:10:04 +0530

    fix lint try

--- quester/insights/src/x_ai/xplainable_openai.rs
+++ quester/insights/src/x_ai/xplainable_openai.rs
@@ -104,7 +104,8 @@ impl InsightRunner for XAIRunner {
 	async fn run_stream(
 		&self,
 		input: Pin<Box<dyn Stream<Item = InsightInput> + Send + 'static>>,
-	) -> InsightResult<Pin<Box<dyn Stream<Item = InsightResult<InsightOutput>> + Send + 'static>>> {
+	) -> InsightResult<Pin<Box<dyn Stream<Item = InsightResult<InsightOutput>> + Send + 'static>>> 
+	{
 		let stream = stream! {
 			pin_mut!(input);
 			while let Some(input) = input.next().await {
