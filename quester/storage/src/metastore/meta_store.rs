3ebadda197699dfefd3d06045c4a8579a015790e
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-07-10 09:00:41 -0500

    metastore refactor

--- quester/storage/src/metastore/meta_store.rs
+++ quester/storage/src/metastore/meta_store.rs
@@ -124,17 +124,21 @@ impl Storage for MetaStore {
 	}
 
 	/// Get all SemanticPipeline ran by this node
-	async fn get_all_pipelines(&self) -> StorageResult<Vec<String>> {
+	async fn get_all_pipelines(&self) -> StorageResult<Vec<SemanticPipelineRequest>> {
 		Ok(Vec::new())
 	}
 
 	/// Set SemanticPipeline ran by this node
-	async fn set_pipeline(&self, _pipeline: &String) -> StorageResult<()> {
+		async fn set_pipeline(&self, pipeline_id: &String, pipeline: SemanticPipelineRequest)
+		-> StorageResult<()> {
 		Ok(())
 	}
 
 	/// Get semantic pipeline by id
-	async fn get_pipeline(&self, _pipeline_id: &String) -> StorageResult<Option<String>> {
+	async fn get_pipeline(
+		&self,
+		_pipeline_id: &String,
+	) -> StorageResult<Option<SemanticPipelineRequest>> {
 		Ok(None)
 	}
 
@@ -144,32 +148,32 @@ impl Storage for MetaStore {
 	}
 
 	/// Get all Discovery sessions ran by this node
-	async fn get_all_discovery_sessions(&self) -> StorageResult<Vec<String>> {
+	async fn get_all_discovery_sessions(&self) -> StorageResult<Vec<DiscoverySessionRequest>> {
 		Ok(Vec::new())
 	}
 
 	/// Set Discovery session ran by this node
-	async fn set_discovery_session(&self, _session: &String) -> StorageResult<()> {
+	async fn set_discovery_session(&self, _session_id: &String, _session: DiscoverySessionRequest) -> StorageResult<()> {
 		Ok(())
 	}
 
 	/// Get Discovery session by id
-	async fn get_discovery_session(&self, _session_id: &String) -> StorageResult<Option<String>> {
+	async fn get_discovery_session(&self, _session_id: &String) -> StorageResult<Option<DiscoverySessionRequest>> {
 		Ok(None)
 	}
 
 	/// Get all Insight sessions ran by this node
-	async fn get_all_insight_sessions(&self) -> StorageResult<Vec<String>> {
+	async fn get_all_insight_sessions(&self) -> StorageResult<Vec<InsightAnalystRequest>> {
 		Ok(Vec::new())
 	}
 
 	/// Set Insight session ran by this node
-	async fn set_insight_session(&self, _session: &String) -> StorageResult<()> {
+	async fn set_insight_session(&self, _session_id: &String, _session: InsightAnalystRequest) -> StorageResult<()> {
 		Ok(())
 	}
 
 	/// Get Insight session by id
-	async fn get_insight_session(&self, _session_id: &String) -> StorageResult<Option<String>> {
+	async fn get_insight_session(&self, _session_id: &String) -> StorageResult<Option<InsightAnalystRequest>> {
 		Ok(None)
 	}
 }
