e2908e6603863ada59d6990ee0071d6dd45c1bfb
ngupta10 <ngupta10.slb@gmail.com>
2024-07-12 00:26:16 +0530

    implemented insight table logic

--- quester/insights/src/x_ai/xplainable_openai.rs
+++ quester/insights/src/x_ai/xplainable_openai.rs
@@ -261,6 +261,59 @@ impl InsightRunner for XAIRunner {
 									},
 								}
 							}
+							// Summarize the results using OpenAI
+							let unique_sentences_vec = unique_sentences(&all_discovered_data);
+							let numbered_sentences: Vec<String> = unique_sentences_vec
+								.iter()
+								.enumerate()
+								.map(|(i, s)| format!("{}. {}", i + 1, s))
+								.collect();
+							let context = numbered_sentences.join("\n");
+							let prompt = format!(
+                            "Below is the context which was discovered during graph traversal for the user query. Summarize the key findings from the context provided that directly answer the user query.
+                        
+                        User Query:
+                        {}
+                        
+                        Context:
+                        {}
+                        ",
+                            query,
+                            context
+                        );
+
+							let human_message = vec![Message::new_human_message(&prompt)];
+
+							let summary = self.llm.generate(&human_message).await.map_err(|e| {
+								InsightError::new(
+									InsightErrorKind::Internal,
+									anyhow::anyhow!("Failed to generate summary: {:?}", e).into(),
+								)
+							})?;
+
+							// Extract the generation text and replace `\n` with actual line breaks
+							let generation_text = summary.generation.replace("\\n", "\n");
+
+							// Insert the generated insight knowledge into storage
+							storage
+								.insert_insight_knowledge(
+									Some(query.to_string()),
+									Some(session_id.to_string()),
+									Some(generation_text.clone()),
+								)
+								.await
+								.map_err(|e| {
+									InsightError::new(
+										InsightErrorKind::Internal,
+										anyhow::anyhow!(
+											"Failed to insert insight knowledge: {:?}",
+											e
+										)
+										.into(),
+									)
+								})?;
+
+							return Ok(InsightOutput { data: Value::String(generation_text) });
 						},
 						Err(e) => error!("Error retrieving discovered data: {:?}", e),
 					}
@@ -268,40 +321,11 @@ impl InsightRunner for XAIRunner {
 			}
 		}
 
-		// Summarize the results using OpenAI
-		let unique_sentences_vec = unique_sentences(&all_discovered_data);
-		let numbered_sentences: Vec<String> = unique_sentences_vec
-			.iter()
-			.enumerate()
-			.map(|(i, s)| format!("{}. {}", i + 1, s))
-			.collect();
-		let context = numbered_sentences.join("\n");
-		let prompt = format!(
-			"Below is the context which was discovered during graph traversal for the user query. Summarize the key findings from the context provided that directly answer the user query.
-		
-		User Query:
-		{}
-		
-		Context:
-		{}
-		",
-			query, // Replace with the actual user query
-			context
-		);
-
-		let human_message = vec![Message::new_human_message(&prompt)];
-
-		let summary = self.llm.generate(&human_message).await.map_err(|e| {
-			InsightError::new(
-				InsightErrorKind::Internal,
-				anyhow::anyhow!("Failed to generate summary: {:?}", e).into(),
-			)
-		})?;
-
-		// Extract the generation text and replace `\n` with actual line breaks
-		let generation_text = summary.generation.replace("\\n", "\n");
-
-		Ok(InsightOutput { data: Value::String(generation_text) })
+		// If no results are found, return an error or a default value
+		Err(InsightError::new(
+			InsightErrorKind::NotFound,
+			anyhow::anyhow!("No relevant insights found").into(),
+		))
 	}
 
 	async fn run_stream<'life0>(
@@ -314,166 +338,182 @@ impl InsightRunner for XAIRunner {
 		let llm = self.llm.clone();
 
 		let stream = Box::pin(stream! {
-			pin_mut!(input);
-			while let Some(input) = input.next().await {
-				error!("Inside Run Stream-----------{:?}", input);
+				pin_mut!(input);
+				while let Some(input) = input.next().await {
+					error!("Inside Run Stream-----------{:?}", input);
 
-				let session_id = match input.data.get("session_id").and_then(Value::as_str) {
-					Some(sid) => sid.to_string(),
-					None => {
-						yield Err(InsightError::new(
-							InsightErrorKind::Internal,
-							anyhow::anyhow!("Session ID is missing").into(),
-						));
-						continue;
-					}
-				};
-				let query = match input.data.get("query").and_then(Value::as_str) {
-					Some(q) => q.to_string(),
-					None => {
-						yield Err(InsightError::new(
-							InsightErrorKind::Internal,
-							anyhow::anyhow!("Query is missing").into(),
-						));
-						continue;
-					}
-				};
+					let session_id = match input.data.get("session_id").and_then(Value::as_str) {
+						Some(sid) => sid.to_string(),
+						None => {
+							yield Err(InsightError::new(
+								InsightErrorKind::Internal,
+								anyhow::anyhow!("Session ID is missing").into(),
+							));
+							continue;
+						}
+					};
+					let query = match input.data.get("query").and_then(Value::as_str) {
+						Some(q) => q.to_string(),
+						None => {
+							yield Err(InsightError::new(
+								InsightErrorKind::Internal,
+								anyhow::anyhow!("Query is missing").into(),
+							));
+							continue;
+						}
+					};
 
-				let embedding_model = match embedding_model.as_ref() {
-					Some(model) => model,
-					None => {
-						yield Err(InsightError::new(
-							InsightErrorKind::Internal,
-							anyhow::anyhow!("Embedding model is not initialized").into(),
-						));
-						continue;
-					}
-				};
-				let embeddings = match embedding_model.embed(vec![query.clone()], None) {
-					Ok(emb) => emb,
-					Err(e) => {
-						yield Err(InsightError::new(
-							InsightErrorKind::Internal,
-							anyhow::anyhow!("Failed to embed query: {:?}", e).into(),
-						));
-						continue;
-					}
-				};
-				let query_embedding = &embeddings[0];
+					let embedding_model = match embedding_model.as_ref() {
+						Some(model) => model,
+						None => {
+							yield Err(InsightError::new(
+								InsightErrorKind::Internal,
+								anyhow::anyhow!("Embedding model is not initialized").into(),
+							));
+							continue;
+						}
+					};
+					let embeddings = match embedding_model.embed(vec![query.clone()], None) {
+						Ok(emb) => emb,
+						Err(e) => {
+							yield Err(InsightError::new(
+								InsightErrorKind::Internal,
+								anyhow::anyhow!("Failed to embed query: {:?}", e).into(),
+							));
+							continue;
+						}
+					};
+					let query_embedding = &embeddings[0];
 
-				let mut all_discovered_data: Vec<(i32, String, String, String, String, String, String, f32)> = Vec::new();
-				for (event_type, storages) in config.event_storages.iter() {
-					if *event_type == EventType::Vector {
-						for storage in storages.iter() {
-							let search_results = storage.similarity_search_l2(
-								config.discovery_session_id.clone(),
-								query.clone(),
-								config.semantic_pipeline_id.clone(),
-								query_embedding,
-								10,
-								0,
-							).await;
+					let mut all_discovered_data: Vec<(i32, String, String, String, String, String, String, f32)> = Vec::new();
+					for (event_type, storages) in config.event_storages.iter() {
+						if *event_type == EventType::Vector {
+							for storage in storages.iter() {
+								let search_results = storage.similarity_search_l2(
+									config.discovery_session_id.clone(),
+									query.clone(),
+									config.semantic_pipeline_id.clone(),
+									query_embedding,
+									10,
+									0,
+								).await;
 
-							match search_results {
-								Ok(results) => {
-									let filtered_results = get_top_k_pairs(results.clone(), 2);
-									let traverser_results_1 = storage.traverse_metadata_table(filtered_results.clone()).await;
+								match search_results {
+									Ok(results) => {
+										let filtered_results = get_top_k_pairs(results.clone(), 2);
+										let traverser_results_1 = storage.traverse_metadata_table(filtered_results.clone()).await;
 
-									if self.previous_query_results.read().unwrap().is_empty()
-										|| *self.previous_session_id.read().unwrap() != session_id
-									{
-										match &traverser_results_1 {
-											Ok(ref traverser_results) => {
-												*self.previous_query_results.write().unwrap() = serde_json::to_string(traverser_results).unwrap_or_default();
-												*self.previous_filtered_results.write().unwrap() = filtered_results.clone();
-												*self.previous_session_id.write().unwrap() = session_id.to_string();
-												all_discovered_data.extend(traverser_results.clone());
+										if self.previous_query_results.read().unwrap().is_empty()
+											|| *self.previous_session_id.read().unwrap() != session_id
+										{
+											match &traverser_results_1 {
+												Ok(ref traverser_results) => {
+													*self.previous_query_results.write().unwrap() = serde_json::to_string(traverser_results).unwrap_or_default();
+													*self.previous_filtered_results.write().unwrap() = filtered_results.clone();
+													*self.previous_session_id.write().unwrap() = session_id.to_string();
+													all_discovered_data.extend(traverser_results.clone());
+												}
+												Err(e) => {
+													error!("Failed to serialize traverser results: {:?}", e);
+												}
 											}
-											Err(e) => {
-												error!("Failed to serialize traverser results: {:?}", e);
-											}
-										}
-									} else {
-										let previous_results: Vec<(
-											i32,
-											String,
-											String,
-											String,
-											String,
-											String,
-											String,
-											f32,
-										)> = serde_json::from_str(&self.previous_query_results.read().unwrap()).unwrap_or_default();
-										let current_results = match &traverser_results_1 {
-											Ok(ref traverser_results) => traverser_results.clone(),
-											Err(e) => {
-												error!("Failed to search for similar documents in traverser: {:?}", e);
-												vec![]
-											}
-										};
-
-										let formatted_output_1 = extract_unique_pairs(current_results.clone(), filtered_results.clone());
-										let formatted_output_2 = extract_unique_pairs(previous_results.clone(), self.previous_filtered_results.read().unwrap().clone());
-
-										let results_intersection = find_intersection(formatted_output_1.clone(), formatted_output_2.clone());
-
-										let final_traverser_results = if results_intersection.is_empty() {
-											*self.previous_filtered_results.write().unwrap() = formatted_output_1.clone();
-											storage.traverse_metadata_table(formatted_output_1.clone()).await
 										} else {
-											*self.previous_filtered_results.write().unwrap() = results_intersection.clone();
-											storage.traverse_metadata_table(results_intersection.clone()).await
-										};
+											let previous_results: Vec<(
+												i32,
+												String,
+												String,
+												String,
+												String,
+												String,
+												String,
+												f32,
+											)> = serde_json::from_str(&self.previous_query_results.read().unwrap()).unwrap_or_default();
+											let current_results = match &traverser_results_1 {
+												Ok(ref traverser_results) => traverser_results.clone(),
+												Err(e) => {
+													error!("Failed to search for similar documents in traverser: {:?}", e);
+													vec![]
+												}
+											};
 
-										match final_traverser_results.clone() {
-											Ok(ref results) => {
-												*self.previous_query_results.write().unwrap() = serde_json::to_string(results).unwrap_or_default();
-												all_discovered_data.extend(results.clone());
-											}
-											Err(e) => {
-												error!("Failed to search for similar documents in traverser: {:?}", e);
+											let formatted_output_1 = extract_unique_pairs(current_results.clone(), filtered_results.clone());
+											let formatted_output_2 = extract_unique_pairs(previous_results.clone(), self.previous_filtered_results.read().unwrap().clone());
+
+											let results_intersection = find_intersection(formatted_output_1.clone(), formatted_output_2.clone());
+
+											let final_traverser_results = if results_intersection.is_empty() {
+												*self.previous_filtered_results.write().unwrap() = formatted_output_1.clone();
+												storage.traverse_metadata_table(formatted_output_1.clone()).await
+											} else {
+												*self.previous_filtered_results.write().unwrap() = results_intersection.clone();
+												storage.traverse_metadata_table(results_intersection.clone()).await
+											};
+
+											match final_traverser_results.clone() {
+												Ok(ref results) => {
+													*self.previous_query_results.write().unwrap() = serde_json::to_string(results).unwrap_or_default();
+													all_discovered_data.extend(results.clone());
+												}
+												Err(e) => {
+													error!("Failed to search for similar documents in traverser: {:?}", e);
+												}
 											}
 										}
-									}
+									// Summarize the results using OpenAI
+									let unique_sentences_vec = unique_sentences(&all_discovered_data);
+									let numbered_sentences: Vec<String> = unique_sentences_vec.iter().enumerate().map(|(i, s)| format!("{}. {}", i + 1, s)).collect();
+									let context = numbered_sentences.join("\n");
+									let prompt = format!(
+										"Below is the context which was discovered during graph traversal for the user query. Summarize the key findings from the context provided that directly answer the user query.
+                                    
+                                    User Query:
+                                    {}
+                                    
+                                    Context:
+                                    {}
+                                    ",
+										query,
+										context
+									);
+
+									let human_message = vec![Message::new_human_message(&prompt)];
+
+									let summary = match llm.generate(&human_message).await {
+										Ok(sum) => sum,
+										Err(e) => {
+											yield Err(InsightError::new(
+												InsightErrorKind::Internal,
+												anyhow::anyhow!("Failed to generate summary: {:?}", e).into(),
+											));
+											continue;
+										}
+									};
+
+									let generation_text = summary.generation.replace("\\n", "\n");
+
+									// Insert the generated insight knowledge into storage
+									match storage.insert_insight_knowledge(
+										Some(query.to_string()),
+										Some(session_id.to_string()),
+										Some(generation_text.clone())
+									).await {
+										Ok(_) => {},
+										Err(e) => {
+											yield Err(InsightError::new(
+												InsightErrorKind::Internal,
+												anyhow::anyhow!("Failed to insert insight knowledge: {:?}", e).into(),
+											));
+											continue;
+										}
+									};
+
+									yield Ok(InsightOutput { data: Value::String(generation_text) });
 								}
 								Err(e) => error!("Error retrieving discovered data: {:?}", e),
 							}
 						}
 					}
 				}
-
-				let unique_sentences_vec = unique_sentences(&all_discovered_data);
-				let numbered_sentences: Vec<String> = unique_sentences_vec.iter().enumerate().map(|(i, s)| format!("{}. {}", i + 1, s)).collect();
-				let context = numbered_sentences.join("\n");
-				let prompt = format!(
-					"Below is the context which was discovered during graph traversal for the user query. Summarize the key findings from the context provided that directly answer the user query.
-                
-                User Query:
-                {}
-                
-                Context:
-                {}
-                ",
-					query,
-					context
-				);
-
-				let human_message = vec![Message::new_human_message(&prompt)];
-
-				let summary = match llm.generate(&human_message).await {
-					Ok(sum) => sum,
-					Err(e) => {
-						yield Err(InsightError::new(
-							InsightErrorKind::Internal,
-							anyhow::anyhow!("Failed to generate summary: {:?}", e).into(),
-						));
-						continue;
-					}
-				};
-
-				let generation_text = summary.generation.replace("\\n", "\n");
-
-				yield Ok(InsightOutput { data: Value::String(generation_text) });
 			}
 		});
 
