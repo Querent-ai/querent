e5d04520d3abded67ae6b411d09374b6876e9522
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-08-02 20:09:50 -0500

    add tauri dependencies

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -1,4 +1,12 @@
+use once_cell::sync::OnceCell;
+use parking_lot::Mutex;
 use proto::NodeConfig;
+use std::sync::atomic::AtomicBool;
+use tauri::AppHandle;
+
+pub static APP_HANDLE: OnceCell<AppHandle> = OnceCell::new();
+pub static ALWAYS_ON_TOP: AtomicBool = AtomicBool::new(false);
+pub static CPU_VENDOR: Mutex<String> = Mutex::new(String::new());
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run(node_config: NodeConfig) {
