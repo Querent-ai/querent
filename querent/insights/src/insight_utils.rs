325f9a0bf2123d9c5873eee2fcd5c5e159f16487
ngupta10 <ngupta10.slb@gmail.com>
2024-08-12 18:40:31 +0530

    added test to insight utils

--- querent/insights/src/insight_utils.rs
+++ querent/insights/src/insight_utils.rs
@@ -2,6 +2,7 @@ use fastembed::{RerankInitOptions, RerankerModel, TextRerank};
 use std::collections::HashMap;
 use tracing::error;
 
+/// Function to get unique contexts
 pub fn unique_sentences(
 	discovered_knowledge: &[(String, String, String, String, String, String, String, f32)],
 ) -> (Vec<String>, usize) {
@@ -59,6 +60,7 @@ pub fn rerank_documents(query: &str, documents: Vec<String>) -> Option<Vec<(Stri
 	)
 }
 
+/// Functions to split a group of sentences into a set of 10 sentences
 pub fn split_sentences(sentences: Vec<String>) -> Vec<Vec<String>> {
 	let mut result = Vec::new();
 	let mut chunk = Vec::new();
@@ -75,4 +77,81 @@ pub fn split_sentences(sentences: Vec<String>) -> Vec<Vec<String>> {
 	}
 
 	result
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_unique_sentences() {
+		let discovered_knowledge = vec![
+			(
+				"doc1".to_string(),
+				"type1".to_string(),
+				"Entity1".to_string(),
+				"Entity2".to_string(),
+				"relation1".to_string(),
+				"This is a sentence.".to_string(),
+				"additional".to_string(),
+				0.9,
+			),
+			(
+				"doc2".to_string(),
+				"type2".to_string(),
+				"Entity1".to_string(),
+				"Entity3".to_string(),
+				"relation2".to_string(),
+				"This is another sentence.".to_string(),
+				"additional".to_string(),
+				0.8,
+			),
+			(
+				"doc3".to_string(),
+				"type3".to_string(),
+				"Entity1".to_string(),
+				"Entity2".to_string(),
+				"relation3".to_string(),
+				"This is a sentence.".to_string(),
+				"additional".to_string(),
+				0.85,
+			),
+		];
+
+		let (unique_sentences, count) = unique_sentences(&discovered_knowledge);
+
+		assert_eq!(unique_sentences.len(), 2);
+		assert_eq!(count, 2);
+		assert!(unique_sentences
+			.contains(&"Entities: Entity1 and Entity2, Sentence: This is a sentence.".to_string()));
+		assert!(unique_sentences.contains(
+			&"Entities: Entity1 and Entity3, Sentence: This is another sentence.".to_string()
+		));
+	}
+
+	#[test]
+	fn test_split_sentences() {
+		let sentences = vec![
+			"Sentence 1".to_string(),
+			"Sentence 2".to_string(),
+			"Sentence 3".to_string(),
+			"Sentence 4".to_string(),
+			"Sentence 5".to_string(),
+			"Sentence 6".to_string(),
+			"Sentence 7".to_string(),
+			"Sentence 8".to_string(),
+			"Sentence 9".to_string(),
+			"Sentence 10".to_string(),
+			"Sentence 11".to_string(),
+		];
+
+		let split = split_sentences(sentences.clone());
+
+		assert_eq!(split.len(), 2);
+		assert_eq!(split[0].len(), 10);
+		assert_eq!(split[1].len(), 1);
+		assert_eq!(split[0][0], "Sentence 1".to_string());
+		assert_eq!(split[0][9], "Sentence 10".to_string());
+		assert_eq!(split[1][0], "Sentence 11".to_string());
+	}
 }
