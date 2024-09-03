b6e1af6fd4aecec035a6b176e40f8fb9edbb5ae2
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-09-03 11:40:44 -0500

    try this

--- tauri/rian/src-tauri/build.rs
+++ tauri/rian/src-tauri/build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    // This line adds the "+crt-static" flag to the rustc compiler when building for Windows.
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-cfg=target_feature=\"crt-static\"");
+    }
     tauri_build::build()
 }
