f51016a19d58dae2f7b9b337b9836f5f9e1a466b
Puneet Saraswat <61435908+saraswatpuneet@users.noreply.github.com>
2024-09-27 10:01:54 -0500

    Embedded PGVector with Postgres: Possible Replace of SurrealDB (#267)
    * base work to refactor pgvector for pgembed
    
    * try stable toolchain for future release?
    
    * set embedded pg vector
    
    * finalize embedded pgvector
    
    * finalize a version for PGEMbed
    
    * fixed typo and pgembed subdirectory
    
    * revert back to vectors
    
    * working insertions plus test
    
    * works similarity with unittest
    
    * remove surreal db
    
    * working retriever
    
    * working discovery data insertion and retrieval with tests
    
    * all functions working (retriever, traverser, insights)
    
    * set postgres as singleton on start
    
    * Use sync psql and handle start at tauri side
    
    * Added alter system commands for tauri (main issue)
    
    * try persistence settings (#273)
    
    * try persistence settings
    
    * set it for node level embedded too
    
    * fixes
    
    * revert pool timeout change(from src tauri)
    
    * final fix to make tauri reopen without postgres already running issue
    
    * fix
    
    ---------
    
    Co-authored-by: Nishant Gupta <ngupta10.slb@gmail.com>
    
    ---------
    
    Co-authored-by: Nishant Gupta <ngupta10.slb@gmail.com>

---
+++ querent/storage/src/vector/pgvector/migrations/2024-09-22-155008_pgembed_querent_semantic/up.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS discovered_knowledge(
+    id SERIAL PRIMARY KEY,
+    doc_id VARCHAR,
+    doc_source VARCHAR,
+    sentence TEXT,
+    subject TEXT,
+    object TEXT,
+    cosine_distance FLOAT8,
+    query_embedding vector(384),
+    query VARCHAR,
+    session_id VARCHAR,
+    score FLOAT8, 
+    collection_id VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS embedded_knowledge(
+    id SERIAL PRIMARY KEY,
+    embeddings vector(384),
+    score FLOAT4,
+    event_id VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS insight_knowledge (
+    id SERIAL PRIMARY KEY,
+    query VARCHAR,
+    session_id VARCHAR,
+    response VARCHAR
+
+);
+
+CREATE TABLE IF NOT EXISTS semantic_knowledge (
+    id SERIAL PRIMARY KEY,
+    subject VARCHAR,
+    subject_type VARCHAR,
+    object VARCHAR,
+    object_type VARCHAR,
+    sentence TEXT,
+    document_id VARCHAR,
+    document_source VARCHAR,
+    collection_id VARCHAR,
+    image_id VARCHAR,
+    event_id VARCHAR,
+    source_id VARCHAR
+);
+
--- querent/storage/src/vector/pgvector/mod.rs
+++ querent/storage/src/vector/pgvector/mod.rs
@@ -1,4 +1,8 @@
+pub mod pg_embed;
+pub use pg_embed::*;
 pub mod pg_vector;
 pub use pg_vector::*;
 pub mod utils;
 pub use utils::*;
+pub mod models;
+pub use models::*;
