4b0e7ffe515ee33e0b6dce683cb4dcdfcbf2ecfe
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-09-30 19:35:37 -0500

    correctly configure libqp build time

--- querent/node/build.rs
+++ querent/node/build.rs
@@ -127,20 +127,15 @@ fn download_windows_npcap_sdk() {
 #[cfg(target_os = "windows")]
 fn setup_libpq_vcpkg() {
 	// Try to configure libpq using vcpkg
-	match vcpkg::find_package("libpq") {
-		Ok(_) => {
-			// vcpkg setup succeeded
-			println!("cargo:rustc-link-lib=libpq");
+	vcpkg::probe_package("libpq")
+		.map(|_| {
+			// found libpq which depends on openssl
+			vcpkg::Config::new().find_package("openssl").ok();
+
 			println!("cargo:rustc-link-lib=crypt32");
 			println!("cargo:rustc-link-lib=gdi32");
 			println!("cargo:rustc-link-lib=user32");
 			println!("cargo:rustc-link-lib=secur32");
-			println!("cargo:rustc-link-lib=shell32");
-			println!("cargo:rustc-link-lib=wldap32");
-		},
-		Err(_) => {
-			// Handle errors if vcpkg setup fails
-			panic!("Failed to configure libpq via vcpkg.");
-		},
-	}
+		})
+		.is_ok()
 }
