0c8f1fd7c994f3d8d24d4520a55868614a9780ff
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-09-30 20:41:04 -0500

    use vcpkg to find libpq

--- querent/node/build.rs
+++ querent/node/build.rs
@@ -126,16 +126,16 @@ fn download_windows_npcap_sdk() {
 
 #[cfg(target_os = "windows")]
 fn setup_libpq_vcpkg() {
-	// Try to configure libpq using vcpkg
-	vcpkg::probe_package("libpq")
+	vcpkg::find_package("libpq")
 		.map(|_| {
 			// found libpq which depends on openssl
 			vcpkg::Config::new().find_package("openssl").ok();
-
 			println!("cargo:rustc-link-lib=crypt32");
 			println!("cargo:rustc-link-lib=gdi32");
 			println!("cargo:rustc-link-lib=user32");
 			println!("cargo:rustc-link-lib=secur32");
+			println!("cargo:rustc-link-lib=shell32");
+			println!("cargo:rustc-link-lib=wldap32");
 		})
 		.is_ok()
 }
--- tauri/rian/src-tauri/build.rs
+++ tauri/rian/src-tauri/build.rs
@@ -11,16 +11,16 @@ fn main() {
 
 #[cfg(target_os = "windows")]
 fn setup_libpq_vcpkg() {
-    // Try to configure libpq using vcpkg
-    vcpkg::probe_package("libpq")
+    vcpkg::find_package("libpq")
         .map(|_| {
             // found libpq which depends on openssl
             vcpkg::Config::new().find_package("openssl").ok();
-
             println!("cargo:rustc-link-lib=crypt32");
             println!("cargo:rustc-link-lib=gdi32");
             println!("cargo:rustc-link-lib=user32");
             println!("cargo:rustc-link-lib=secur32");
+            println!("cargo:rustc-link-lib=shell32");
+            println!("cargo:rustc-link-lib=wldap32");
         })
         .is_ok()
 }
