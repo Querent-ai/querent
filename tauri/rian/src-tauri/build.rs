46ffd66443ab5f3f7f30bdb4d01205f85f8e30c3
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-10-08 09:32:27 -0700

    run with installed postgres and link against both static and dynamic

--- tauri/rian/src-tauri/build.rs
+++ tauri/rian/src-tauri/build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    println!("cargo::rustc-link-search=C:\\Program Files\\PostgreSQL\\16\\lib");
+    #[cfg(target_os = "windows")]
+    println!("cargo:rustc-link-lib=libpq");
     tauri_build::build()
 }
