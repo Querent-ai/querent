884be0d6f38e56b0f05513331e62d5f3dbc89c88
Nishant Gupta <ngupta10.slb@gmail.com>
2024-10-18 21:40:34 +0530

    linting

--- tauri/rian/src/routes/(sidebar)/crud/semantic-web/add/TimeSeriesFabric.svelte
+++ tauri/rian/src/routes/(sidebar)/crud/semantic-web/add/TimeSeriesFabric.svelte
@@ -1,1 +1,7 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M5.823 14.177a2 2 0 1 1-1-1l2.354-2.354a2 2 0 1 1 3.646 0l2.354 2.354a2 2 0 0 1 1.646 0l3.354-3.354a2 2 0 1 1 1 1l-3.354 3.354a2 2 0 1 1-3.646 0l-2.354-2.354a2 2 0 0 1-1.646 0z"/></svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"
+	><path
+		fill="currentColor"
+		fill-rule="evenodd"
+		d="M5.823 14.177a2 2 0 1 1-1-1l2.354-2.354a2 2 0 1 1 3.646 0l2.354 2.354a2 2 0 0 1 1.646 0l3.354-3.354a2 2 0 1 1 1 1l-3.354 3.354a2 2 0 1 1-3.646 0l-2.354-2.354a2 2 0 0 1-1.646 0z"
+	/></svg
+>
